64ac67d3bf3989f7e1ad1f1a34652872d7dad932
Dominic <74207434+DominicDern@users.noreply.github.com>
2026-05-09 20:42:54 -0300

    Basic UI setup
    Added iced 1.14.0 for ui development

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{container, row, text};
+use iced::{Element, Theme};
+
+pub fn main() -> iced::Result {
+    iced::application(new, update, view).theme(theme).run()
+}
+
+#[derive(Default)]
+struct State {}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+fn new() -> State {
+    State {}
+}
+
+fn theme(_state: &State) -> Theme {
+    Theme::Nord
+}
+
+fn update(state: &mut State, message: Message) {
+    match message {}
+}
+
+fn view(state: &State) -> Element<'_, Message> {
+    row![container(text!("TEST"))].spacing(10).into()
 }
