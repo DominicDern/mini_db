e5261ae66c0540f3120bdc5e810d03c108a5e715
Dominic <74207434+DominicDern@users.noreply.github.com>
2026-05-10 01:45:00 -0300

    Added the structure for a home page

---
+++ src/ui/views/home.rs
@@ -0,0 +1,9 @@
+use crate::ui::state::App;
+
+use crate::ui::messages::Message;
+use iced::Element;
+use iced::widget::{container, row, text};
+
+pub fn view(_state: &App) -> Element<'_, Message> {
+    row![container(text!("TEST"))].spacing(10).into()
+}
--- src/ui/views/mod.rs
+++ src/ui/views/mod.rs
@@ -1,10 +1,13 @@
+mod home;
+
 use crate::ui::messages::Message;
-use crate::ui::state::App;
+use crate::ui::state::{App, Page};
 
 use iced::Element;
-use iced::widget::{container, row, text};
 
 // View dispatcher
-pub fn view(_state: &App) -> Element<'_, Message> {
-    row![container(text!("TEST"))].spacing(10).into()
+pub fn view(state: &App) -> Element<'_, Message> {
+    match state.page {
+        Page::Home => home::view(state),
+    }
 }
