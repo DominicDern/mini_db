5e97a75583096fca2eaa41617744f22811233cde
Dominic <74207434+DominicDern@users.noreply.github.com>
2026-05-10 12:42:12 -0300

    Added basic messages for UI, Logic, and DB

--- src/ui/messages.rs
+++ src/ui/messages.rs
@@ -1,2 +1,15 @@
 #[derive(Debug, Clone)]
-pub enum Message {}
+pub enum Message {
+    UI(UIMessage),
+    Logic(LogicMessage),
+    DB(DBMessage),
+}
+
+#[derive(Debug, Clone)]
+pub enum UIMessage {}
+
+#[derive(Debug, Clone)]
+pub enum LogicMessage {}
+
+#[derive(Debug, Clone)]
+pub enum DBMessage {}
